c53774793af0fa05906fdfd6f962975015ec866a
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-29 14:51:24 +0100

    validations user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :building
+  has_many :messages, :documents, :demands, :materials
+
+  validates :user_name, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
