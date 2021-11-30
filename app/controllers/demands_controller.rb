21db128589b99184aaea39b91249b692afb29542
Ricardo Costa <ra.silva.dcosta@gmail.com>
2021-11-30 10:41:30 +0100

    demand views index and new

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -1,15 +1,26 @@
 class DemandsController < ApplicationController
+  # before_action :new, :create
+
   def index
-    @building = Building.find(1)
-    @demands =  Demand.all
+    @demands = Demand.all
   end
 
   def new
+    @building = Building.find(7)
     @demand = Demand.new
   end
 
   def create
+    @building = Building.find(7)
     @demand = Demand.new(demand_params)
+    @demand.building = @building
+    @demand.requester_id = current_user.id
+
+    if @demand.save!
+      redirect_to building_demands_path(@building)
+    else
+      render :new
+    end
   end
 
   private
