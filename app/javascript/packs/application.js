1f699f282e7b94200ea65a9ea87ff61436142565
Alexandre Picard <alex.picard@bluewin.ch>
2021-12-07 13:35:14 +0100

    Scrolling messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,11 +29,13 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initBuildingCable } from '../channels/building_channel';
 import { showNavbar, menuItem } from '../components/navBar';
+import { showFooter} from '../components/messages';
 import { simpleCalendar } from '../components/simpleCalendar'
 
 document.addEventListener('turbolinks:load', () => {
   initBuildingCable();
   showNavbar();
+  showFooter();
   menuItem();
   simpleCalendar();
 });
